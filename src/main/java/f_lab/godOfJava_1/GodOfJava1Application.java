2a9d8cf9e7b2529422f45530cf05448831e2a3f2
gdrffg <gdrffg@gmail.com>
2024-10-22 13:50:08 +0900

    first commit

---
+++ src/main/java/f_lab/godOfJava_1/GodOfJava1Application.java
@@ -0,0 +1,13 @@
+package f_lab.godOfJava_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodOfJava1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodOfJava1Application.class, args);
+	}
+
+}
---
+++ src/test/java/f_lab/godOfJava_1/GodOfJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package f_lab.godOfJava_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodOfJava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
