fa9deac6beafe328fe1535e8f5edc3cc28f9dd75
gdrffg <gdrffg@gmail.com>
2024-11-25 16:08:28 +0900

    add: collection framework-2

--- src/main/java/f_lab/godOfJava_1/collectionsFramework/MyVector.java
+++ src/main/java/f_lab/godOfJava_1/collectionsFramework/MyVector.java
@@ -2,7 +2,7 @@ package f_lab.godOfJava_1.collectionsFramework;
 
 import java.util.*;
 
-public class MyVector implements List {
+public abstract class MyVector implements List {
 
     Object[] data = null; // 객체를 담기 위한 객체배열을 선언한다.
     int capacity = 0; // 용량
@@ -94,7 +94,7 @@ public class MyVector implements List {
         size = 0;
     }
 
-    @Override
+
     public Object[] toArray() {
         Object[] result = new Object[size];
         System.arraycopy(data, 0, result, 0, size);
